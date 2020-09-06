2528024f98bb1a1bae04c61ef3223b0ca290aa99
ruofish <1786392630@qq.com>
2020-09-06 08:58:10 +0800

    add gittest dir

---
+++ gitTest/index.ts
@@ -0,0 +1,18 @@
+function test(name:string):string{
+    return `${name}`
+}
+
+test('111')
+
+const a:number = 1
+
+function Persion(name:string,age:number):string{
+    console.log('person')
+    return `${name}${age}`
+} 
+
+let len:string;
+
+(function() {
+    console.log('333')
+})()
---
+++ gitTest/js/index.js
@@ -0,0 +1,14 @@
+"use strict";
+function test(name) {
+    return "" + name;
+}
+test('111');
+var a = 1;
+function Persion(name, age) {
+    console.log('person');
+    return "" + name + age;
+}
+var len;
+(function () {
+    console.log('333');
+})();
